55613201afffe8a9d2e0a05762cfbf53868e7660
Pan i Wladca <paniwladca@malpa.zsrr>
2018-03-20 23:00:06 +0100

    Saper 1.0

---
+++ Mapa.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+public class Mapa
+{
+	private boolean[] mapa = new boolean[36];
+	private Random losuj = new Random();
+	private int miny = 0;
+	
+	public Mapa() 
+	{
+		losujMape();
+	}
+	
+	public void losujMape()
+	{
+				for (int i = 0; i<mapa.length; i++)
+		{
+		if (losuj.nextInt(20)>1) mapa[i]=false;
+		else mapa[i]=true;
+		}
+	}
+
+	public boolean getMina(int i)
+	{
+		return mapa[i];
+	}
+	
+	public int getMiny()
+	{
+		miny = 0;
+		for (int i = 0; i < mapa.length; i++)
+		{
+			if (mapa[i]) miny++;
+		}
+		return miny;
+	}
+}
